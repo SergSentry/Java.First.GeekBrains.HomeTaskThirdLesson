feb5f2ab56bba6dac8d9ae2aa27a9cc887ca27b2
Serg <rtfm.faq@mail.ru>
2018-07-19 21:25:20 +0300

    ThirdHomeWork: Создан пустой проект в IntelliJ IDEA, и прописан метод main()

---
+++ src/ru/JavaHomeTask/ThirdLesson/Programm.java
@@ -0,0 +1,15 @@
+package ru.JavaHomeTask.ThirdLesson;
+
+/**
+ *  Класс программы домашнего задания третьего урока
+ */
+public class Programm {
+    /**
+     * Точка входа в приложение
+     *
+     * @param args параметры командной строки
+     */
+    public static void main(String[] args) {
+
+    }
+}
